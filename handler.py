7b8b163d2274e006a18e7f3c6794e0b9b00b5fed
Luis Esteban <rodriguezjluis0@gmail.com>
2018-11-02 17:54:46 -0600

    add YELP_APP_KEY

--- handler.py
+++ handler.py
@@ -1,15 +1,22 @@
 import json
-
+import os
 
 def get_restaurants(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+
+    YELP_APP_KEY = os.getenv('YELP_APP_KEY', None)
+
+    if YELP_APP_KEY is not None:
+        return {
+            "statusCode": 200,
+            "body": {
+                'error': True,
+                'msg': 'Provide correct YELP_APP_KEY'
+            }
+        }
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": json.dumps(event)
     }
 
     return response
