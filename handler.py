bf8b0cdf8bcbea30ea3757213e52b5df7d1d3084
Luis Esteban <rodriguezjluis0@gmail.com>
2018-11-02 20:53:24 -0600

    update db writing

--- handler.py
+++ handler.py
@@ -5,6 +5,8 @@ import requests as req
 import urllib
 import psycopg2
 
+from psycopg2.extensions import AsIs
+
 
 YELP_APP_KEY = os.getenv('YELP_APP_KEY', None)
 YELP_API_ENDPOINT = 'https://api.yelp.com/v3'
@@ -24,8 +26,20 @@ def get_restaurants(search_term, latitude, longitude, radius=0):
         headers=HEADERS,
         params=PARAMS
     )
+    restaurants = response.json()['businesses']
 
-    return response.json()
+    if restaurants.__len__() > 0:
+        return [{
+            'id': x['id'],
+            'name': x['name'],
+            'rating': x['rating'],
+            'latitude': x['coordinates']['latitude'],
+            'longitude': x['coordinates']['longitude'],
+            'phone': x['phone'],
+
+        } for x in restaurants]
+
+    return restaurants
 
 
 def write_json_to_db(obj, table):
@@ -36,16 +50,24 @@ def write_json_to_db(obj, table):
         os.getenv('DB_PASSWD', ''),
     )
     conn = psycopg2.connect(connect_str)
+    conn.autocommit = True
     cursor = conn.cursor()
 
     sql_statement = """
-INSERT INTO {table}
-SELECT *
-FROM   json_populate_recordset(null::{table},
-          json json.dumps({obj}));
-    """.format(table=table, obj=obj)
+INSERT INTO {table} (yelp_id, name, rating, latitude, longitude, phone)
+VALUES ('{yelp_id}', '{name}', {rating}, {latitude}, {longitude}, '{phone}');
+    """
 
-    cursor.execute()
+    for item in obj:
+        cursor.execute(sql_statement.format(
+            table=AsIs(table),
+            yelp_id=item['id'],
+            name=item['name'],
+            rating=item['rating'],
+            latitude=item['latitude'],
+            longitude=item['longitude'],
+            phone=item['phone']
+        ))
     conn.close()
 
 
@@ -53,8 +75,8 @@ def main(event, context):
 
     if YELP_APP_KEY is None:
         return {
-            "statusCode": 200,
-            "body": {
+            'statusCode': 200,
+            'body': {
                 'error': {
                     'code': 'YELP_KEY_MISSING',
                     'description': 'Provide correct YELP_APP_KEY',
@@ -63,12 +85,13 @@ def main(event, context):
         }
 
     restaurants = get_restaurants(**event)
-    if restaurants['businesses'].__len__():
-        write_to_db(items=restaurants, json=True)
+    write_json_to_db(obj=restaurants, table=os.getenv('RESTAURANTS', None))
 
     response = {
-        "statusCode": 200,
-        "body": restaurants,
+        'statusCode': 200,
+        'body': {
+            'restaurants': restaurants,
+        }
     }
 
     return response
