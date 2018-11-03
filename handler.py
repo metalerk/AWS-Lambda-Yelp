a54af00907362eb4c3bdf0296c15a9484a8c7fc6
Luis Esteban <rodriguezjluis0@gmail.com>
2018-11-02 18:59:57 -0600

    get restaurants info

--- handler.py
+++ handler.py
@@ -1,31 +1,48 @@
+import asyncio
 import json
 import os
+import requests as req
+import urllib
 
-def get_restaurants(event, context):
 
-    YELP_APP_KEY = os.getenv('YELP_APP_KEY', None)
+YELP_APP_KEY = os.getenv('YELP_APP_KEY', None)
+YELP_API_ENDPOINT = 'https://api.yelp.com/v3'
+HEADERS = {'Authorization': 'Bearer {}'.format(YELP_APP_KEY)}
+
+
+def get_restaurants(search_term, latitude, longitude, radius=0):
+    PARAMS = {
+        'term': search_term,
+        'latitude': latitude,
+        'longitude': longitude,
+        'radius': radius,
+    }
+    endpoint = '{}/businesses/search'.format(YELP_API_ENDPOINT)
+    response = req.get(
+        endpoint,
+        headers=HEADERS,
+        params=PARAMS
+    )
+
+    return response
+
+def main(event, context):
 
     if YELP_APP_KEY is None:
         return {
             "statusCode": 200,
             "body": {
-                'error': True,
-                'msg': 'Provide correct YELP_APP_KEY'
+                'error': {
+                    'code': 'YELP_KEY_MISSING',
+                    'description': 'Provide correct YELP_APP_KEY',
+                }
             }
         }
 
+    restaurants = get_restaurants(**event)
     response = {
         "statusCode": 200,
-        "body": json.dumps(event)
+        "body": restaurants.json()
     }
 
     return response
-
-    ***REMOVED*** Use this code if you don't use the http event with the LAMBDA-PROXY
-    ***REMOVED*** integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
